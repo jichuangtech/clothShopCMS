96bd0b410ce19425a263bbbd9dad0a10e3553ad0
huangbin <575395982@qq.com>
2017-08-14 09:20:32 +0800

    MOD 利用 redux 进行数据流和 view绑定

--- app/login/LoginRoot.js
+++ app/login/LoginRoot.js
@@ -4,49 +4,74 @@
 var React = require("react");
 import { connect } from 'react-redux';
 import * as LoginAction from '../action/login';
+import { Form, Icon, Input, Button, Checkbox } from 'antd';
+const FormItem = Form.Item;
 
-
-
-var defUser = {
-  email: '211'
-};
-
-class LoginRoot extends React.Component {
-
-
+class NormalLoginForm extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            isLogined: false,
-            isModalVisible: false,
-            email:"",
-            password:""
+            email: "unknown",
+            password: "**"
         }
+    }
+
+    handleSubmit(e) {
+        e.preventDefault();
+        this.props.form.validateFields((err, values) => {
+            if (!err) {
+                console.log('Received values of form: ', values);
+            }
+        });
     }
 
     render() {
-        console.log(" 登录提示 :" + this.props.loginProps.tip + ", status: " + this.props.loginProps.status);
-        if(!(this.props.loginProps.status == "ing")
-            && !(this.props.loginProps.status == "unknown")) {
-            alert(" 登录提示 :" + this.props.loginProps.tip + ", status: " + this.props.loginProps.status);
-        }
+        const { getFieldDecorator } = this.props.form;
+        alert("状态: " + this.props.loginProps.status)
         return (
-            <div>
-                <h2 onClick={this.__onclick.bind(this)}>你好 {this.props.loginProps.tip}</h2>
-            </div>
+            <Form onSubmit={this.handleSubmit} className="login-form">
+                <FormItem>
+                    {getFieldDecorator('userName', {
+                        rules: [{ required: true, message: 'Please input your username!' }],
+                    })(
+                        <Input prefix={<Icon type="user" style={{ fontSize: 13 }} />} placeholder="Username"/>
+                    )}
+                </FormItem>
+                <FormItem>
+                    {getFieldDecorator('password', {
+                        rules: [{ required: true, message: 'Please input your Password!' }],
+                    })(
+                        <Input prefix={<Icon type="lock" style={{ fontSize: 13 }} />} type="password" placeholder="Password" />
+                    )}
+                </FormItem>
+                <FormItem>
+                    {getFieldDecorator('remember', {
+                        valuePropName: 'checked',
+                        initialValue: true,
+                    })(
+                        <Checkbox>Remember me</Checkbox>
+                    )}
+                    <a className="login-form-forgot" href="">Forgot password</a>
+                    <Button type="primary" htmlType="submit" className="login-form-button" onClick={this.__submitBtn.bind(this)}>
+                        Log in
+                    </Button>
+                    Or <a href="">register now!</a>
+                </FormItem>
+            </Form>
         );
-
     }
 
-    __onclick() {
+    __submitBtn() {
         var loginValue = {
-            email: "5753",
-            password:"2232"
+            email:this.state.email,
+            password:this.state.password
         };
         this.props.dispatch(LoginAction.login(loginValue));
     }
-
 }
+
+const WrappedNormalLoginForm = Form.create()(NormalLoginForm);
+
 
 //state 对象，就是经过 combineReducers()创建出来的
 const mapStateToProps = state => ({
@@ -55,4 +80,4 @@ const mapStateToProps = state => ({
 
 
 
-export default connect(mapStateToProps)(LoginRoot);
+export default connect(mapStateToProps)(WrappedNormalLoginForm);
