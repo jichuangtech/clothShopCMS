a251a87e054f40e093dd66e2f969968efe293760
huangbin <575395982@qq.com>
2017-09-09 14:15:46 +0800

    [DEVELOP][MOD] 解决updateProps获取属性失败的问题

--- app/pages/widgets/SelectView.js
+++ app/pages/widgets/SelectView.js
@@ -22,51 +22,81 @@ class SelectView extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            defaultValue: "",
+            optionValue: "",
             optionViews:[]
         }
     }
 
     render() {
+        console.log(" render SelectView optionValue: " + this.state.optionValue);
         return (
             <div className={this.props.className}>
-                <Select value={this.state.defaultValue}
+                <Select value={this.state.optionValue}
                         style={{ width: 120 }}
-                        onChange={this.props.optionChange}>
+                        onChange={this.onOptionChange.bind(this)}>
                     {this.state.optionViews}
                 </Select>
             </div>
         );
     }
 
+    onOptionChange(value) {
+        // //回调外层的组件
+        if(this.props.optionChange != undefined) {
+            this.props.optionChange(value);
+        }
+
+        console.log("SelectView value: " + value  +  ", view.size: " + this.state.optionViews.length);
+        this.setState({
+           optionValue:value
+        });
+    }
+
+    componentWillUpdate() {
+        console.log("SelectView componentWillUpdate");
+    }
+
+    componentDidUpdate() {
+        console.log("SelectView componentDidUpdate");
+
+    }
+
+    componentWillMount() {
+        console.log("SelectView componentWillMount");
+        this.setState({
+            optionValue: this.getOptiontValue(this.props),
+            optionViews: this.getOptionViews(this.props)
+        });
+    }
+
+    getOptiontValue(props) {
+        var value = (props.options.length === 0 ? "加载中..." : props.options[0].value);
+        console.log("SelectView getOptiontValue value: " + value + ", options.size: " + props.options.length);
+        return value;
+    }
+
     getOptionViews(props) {
         var options = props.options;
+        console.log("SelectView getOptionViews options.size: " + options.length);
         var optionViews = [];
 
         for(var index = 0; index < options.length; index++) {
-            var view  = <Option value={options[index].value}>{options[index].title}</Option>;
+            var view  = <Option key={options[index].value}
+                                value={options[index].value}>
+                            {options[index].title}
+                        </Option>;
             optionViews[index] = view;
         }
 
         return optionViews;
     }
 
-    componentWillMount() {
-        this.setState({
-            defaultValue: this.getDefaultValue(this.props),
-            optionViews: this.getOptionViews(this.props)
-        });
-    }
-
-    getDefaultValue(props) {
-        var value = props.options.length == 0 ? "" : props.options[0].value;
-        return value;
-    }
-
     componentWillReceiveProps(nextProps) {
+        console.log(" SelectView componentWillReceiveProps");
+
         this.setState({
-            defaultValue: this.getDefaultValue(nextProps),
-            optionViews: this.getOptionViews(nextProps)
+            optionViews: this.getOptionViews(nextProps),
+            optionValue: this.getOptiontValue(nextProps)
         });
     }
 
@@ -84,7 +114,10 @@ SelectView.defaultProps = {
 export default SelectView;
 
 /*
- 使用React.Component创建组件，需要通过在constructor中调用super()将props传递给React.Component。另外react 0.13之后props必须是不可变的。
- 由于是用ES6 class语法创建组件，其内部只允许定义方法，而不能定义属性，class的属性只能定义在class之外。所以propTypes要写在组件外部。
- 对于之前的getDefaultProps方法，由于props不可变，所以现在被定义为一个属性，和propTypes一样，要定义在class外部。
+ 使用React.Component创建组件，需要通过在constructor中调用super()将props传递给React.Component。
+ 另外react 0.13之后props必须是不可变的。
+ 由于是用ES6 class语法创建组件，其内部只允许定义方法，而不能定义属性，
+ class的属性只能定义在class之外。所以propTypes要写在组件外部。
+ 对于之前的getDefaultProps方法，由于props不可变，
+ 所以现在被定义为一个属性，和propTypes一样，要定义在class外部。
  */
